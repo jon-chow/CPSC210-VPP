fd4ad704ff6a868d64106f79cd17bcbfc6de63eb
T4CT1C4L-C4T <T4CT1C4L-C4T@github.com>
2022-02-05 19:54:14 -0800

    Included file locations for the type "Dog"

--- src/main/model/configurables/FileLocations.java
+++ src/main/model/configurables/FileLocations.java
@@ -6,20 +6,20 @@ public class FileLocations {
     private final String spritesDir = "data/sprites/";
     private final String itemsSpriteDir = spritesDir + "items/";
     private final String exampleSpritesDir = spritesDir + "example_animals/";
-//    private final String dogSpritesDir = spritesDir + "dogs/";
+    private final String dogSpritesDir = spritesDir + "dogs/";
 
     // SPECIFIC FILES
     private final String dataDir = "data/";
     private final String itemsDir = dataDir + "Items.json";
     private final String exampleBreedsDir = dataDir + "petBreeds/ExampleAnimalBreeds.json";
-//    private final String dogBreedsDir = dataDir + "petBreeds/DogBreeds.json";
+    private final String dogBreedsDir = dataDir + "petBreeds/DogBreeds.json";
 
     // GETTERS
     // GENERIC FOLDERS:
     public String getSpritesDir(String dataKey) {
         switch (dataKey.toLowerCase().replaceAll("\\s+","")) {
             case "item": return itemsSpriteDir;
-//            case "dog": return dogSpritesDir;
+            case "dog": return dogSpritesDir;
             default: return exampleSpritesDir;
         }
     }
@@ -29,7 +29,7 @@ public class FileLocations {
     public String getDataDir(String dataKey) {
         switch (dataKey.toLowerCase().replaceAll("\\s+","")) {
             case "item": return itemsDir;
-//            case "dog": return dogBreedsDir;
+            case "dog": return dogBreedsDir;
             default: return exampleBreedsDir;
         }
     }
