8091b4fccae72ef3d9797215cea11f9dc380ed09
T4CT1C4L-C4T <T4CT1C4L-C4T@github.com>
2022-02-05 19:50:42 -0800

    Added shop command keys

---
+++ src/main/ui/configurables/Commands.java
@@ -0,0 +1,21 @@
+package ui.configurables;
+
+public class Commands {
+    // GENERAL COMMANDS
+    public static final String COMMANDS_KEY = "commands";
+    public static final String CONFIRMATION_KEY = "yes";
+    public static final String EXIT_MENU_KEY = "exit";
+
+    // PET MENU COMMANDS
+    public static final String CHECK_PET_KEY = "pet";
+
+    // SHOP MENU COMMANDS
+    public static final String OPEN_SHOP_KEY = "shop";
+    public static final String BUY_KEY = "buy";
+
+    // INVENTORY MENU COMMANDS
+    public static final String OPEN_INVENTORY_KEY = "inventory";
+
+    // PLAYER INFORMATION COMMANDS
+    public static final String VIEW_MONEY_KEY = "money";
+}
