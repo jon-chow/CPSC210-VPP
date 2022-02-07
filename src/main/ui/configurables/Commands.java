9453cfd220cf35b9f82b7fe746d383c58e135c03
T4CT1C4L-C4T <T4CT1C4L-C4T@github.com>
2022-02-07 14:25:28 -0800

    Added inventory and shop command keys

--- src/main/ui/configurables/Commands.java
+++ src/main/ui/configurables/Commands.java
@@ -12,11 +12,13 @@ public class Commands {
     // SHOP MENU COMMANDS
     public static final String OPEN_SHOP_KEY = "shop";
     public static final String VIEW_ITEMS_KEY = "items";
-    public static final String SEPARATOR_KEY = "::";
+    public static final String BUY_ITEM_KEY = "buy";
+    public static final String SHOP_SEPARATOR_KEY = "::";
 
     // INVENTORY MENU COMMANDS
     public static final String OPEN_INVENTORY_KEY = "inventory";
     public static final String GIVE_TO_KEY = "give";
+    public static final String INVENTORY_SEPARATOR_KEY = "::";
 
     // PLAYER INFORMATION COMMANDS
     public static final String VIEW_MONEY_KEY = "money";
