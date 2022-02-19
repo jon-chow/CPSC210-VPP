d0f2304093e054940a43f6c2d36b733adddc71f6
Jon Chow <jon.chowyyc@gmail.com>
2022-02-19 15:30:50 -0800

    replaced JsonReader and JsonWriter

--- src/main/model/persistence/JsonReader.java
+++
@@ -1,6 +0,0 @@
-package model.persistence;
-
-// reads in data from the Persistence.json to the game
-public class JsonReader {
-
-}
--- src/main/model/persistence/JsonWriter.java
+++
@@ -1,6 +0,0 @@
-package model.persistence;
-
-// writes data from the game to Persistence.json
-public class JsonWriter {
-
-}
