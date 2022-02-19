0376ca4b658ba3a44897b34e637808bd414e1054
Jon Chow <jon.chowyyc@gmail.com>
2022-02-19 15:37:30 -0800

    implemented data persistence

--- src/main/ui/app/PixelPetGame.java
+++ src/main/ui/app/PixelPetGame.java
@@ -9,7 +9,7 @@ import model.goodsandservices.Shop;
 import model.pets.*;
 import ui.menus.*;
 
-// class for handling the main game functions and menus
+// class for handling the main game functionalities and menus
 public class PixelPetGame {
     public static final ArrayList<String> ANIMALS_IN_ADOPTION_CLINIC =
             new ArrayList<>(Arrays.asList("Example Animal", "Dog"));
@@ -31,13 +31,13 @@ public class PixelPetGame {
 
     private static final int SECONDS_PER_AGING = 10;
 
+    private int sessionId;
     private int ticksPassed = 0;
     private int secondsPassed = 0;
 
     private boolean endGame = false;
     private Player player;
     private Pet pet;
-
     private ArrayList<Shop> shops;
 
     // EFFECTS: constructs a new PixelPetGame
@@ -45,10 +45,28 @@ public class PixelPetGame {
         Shop shop = ShopMenu.initShop("Kira Kira Pets");
         shops = new ArrayList<>();
         shops.add(shop);
-
         player = NewPlayerMenu.initNewPlayer(STARTING_MONEY);
         pet = AdoptionMenu.initAdoption();
+
+        sessionId = generateSessionId(player.getPlayerName(), pet.getName(), shop.getShopName());
+        System.out.println("Your personal ID for saving and loading the game is " + sessionId + ".\n"
+                            + "Be sure to write it down somewhere or remember it!");
         CommandsMenu.showControls();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: generates a new session id for the current session and returns it
+    private int generateSessionId(String playerName, String petName, String shopName) {
+        String rawKey = (playerName + petName + shopName).replaceAll("\\s","");
+        int encodedKey = 0;
+
+        for (int i = 0; i < rawKey.length(); i++) {
+            encodedKey += rawKey.charAt(i);
+        }
+
+        encodedKey = (int) (encodedKey * System.currentTimeMillis() / 1000L);
+
+        return Math.abs(encodedKey);
     }
 
     // EFFECTS: progresses the game
@@ -86,7 +104,7 @@ public class PixelPetGame {
         Shop theShop = null;
 
         for (Shop shop : shops) {
-            if (shop.getShopName() == shopName) {
+            if (shop.getShopName().equals(shopName)) {
                 theShop = shop;
                 break;
             }
@@ -96,11 +114,48 @@ public class PixelPetGame {
     }
 
     // GETTERS
+    public int getSessionId() {
+        return sessionId;
+    }
+
     public Player getPlayer() {
         return player;
     }
 
     public Pet getPet() {
         return pet;
+    }
+
+    public int getTicksPassed() {
+        return ticksPassed;
+    }
+
+    public int getSecondsPassed() {
+        return secondsPassed;
+    }
+
+    // SETTERS
+    public void setSessionId(int sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void setShops(ArrayList<Shop> shops) {
+        this.shops = shops;
+    }
+
+    public void setTicksPassed(int ticksPassed) {
+        this.ticksPassed = ticksPassed;
+    }
+
+    public void setSecondsPassed(int secondsPassed) {
+        this.secondsPassed = secondsPassed;
     }
 }
