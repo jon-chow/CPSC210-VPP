3fb0698280bca6827ff85b1df6ebedc32411f71f
Jon Chow <jon.chowyyc@gmail.com>
2022-02-24 23:44:50 -0800

    included test files for persistence package

---
+++ src/test/model/persistence/PersistenceWriterTest.java
@@ -0,0 +1,14 @@
+package model.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersistenceWriterTest {
+    @BeforeEach
+    void runBefore() {
+
+    }
+
+}
