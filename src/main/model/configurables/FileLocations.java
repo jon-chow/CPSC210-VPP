7117a44fd76a2da3481633866315c6fcacecb3a1
Jon Chow <70170720+jon-chow@users.noreply.github.com>
2022-03-16 23:31:27 -0700

    added art and font file directories

--- src/main/model/configurables/FileLocations.java
+++ src/main/model/configurables/FileLocations.java
@@ -3,20 +3,26 @@ package model.configurables;
 // class for storing all sprites/json files paths
 // easier to configure directories this way
 public class FileLocations {
-    // PERSISTENCE FILE
-    public static final String persistenceDir = "data/persistence/Persistence.json";
-
-    // GENERIC FOLDERS
-    public static final String spritesDir = "data/sprites/";
-    public static final String itemsSpriteDir = spritesDir + "items/";
-    public static final String exampleSpritesDir = spritesDir + "example_animals/";
-    public static final String dogSpritesDir = spritesDir + "dogs/";
+    // DATA FOLDER
+    public static final String dataDir = "data/";
+    public static final String assetsDir = dataDir + "assets/";
+    public static final String backgroundsDir = assetsDir + "backgrounds/";
+    public static final String iconsDir = assetsDir + "icons/";
+    public static final String spritesDir = assetsDir + "sprites/";
 
     // SPECIFIC FILES
-    public static final String dataDir = "data/";
+    public static final String persistenceDir = dataDir + "persistence/Persistence.json";
     public static final String itemsDir = dataDir + "Items.json";
     public static final String exampleBreedsDir = dataDir + "petBreeds/ExampleAnimalBreeds.json";
     public static final String dogBreedsDir = dataDir + "petBreeds/DogBreeds.json";
+
+    // GUI FILES
+    public static final String gameIconDir = iconsDir + "PixelPet.png";
+//    public static final String gameBGMDir = assetsDir + "";
+
+    public static final String itemsSpriteDir = spritesDir + "items/";
+    public static final String exampleSpritesDir = spritesDir + "example_animals/";
+    public static final String dogSpritesDir = spritesDir + "dogs/";
 
     // GETTERS
     // GENERIC FOLDERS:
