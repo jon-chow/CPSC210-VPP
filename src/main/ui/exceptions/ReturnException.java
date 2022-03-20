b5fe174d8ebd114c23418db1968807a71bc1bb6f
Jon Chow <70170720+jon-chow@users.noreply.github.com>
2022-03-20 15:56:05 -0700

    removed

--- src/main/ui/exceptions/ReturnException.java
+++
@@ -1,9 +0,0 @@
-package ui.exceptions;
-
-// exception class for pressing the return button
-public class ReturnException extends Exception {
-    // EFFECTS: sends an exception if return button was pressed
-    public ReturnException() {
-        super("Return button was pressed!");
-    }
-}
