e74ea56dfc22abc25b4e670aff2abe9f26b4e1f3
Jon Chow <70170720+jon-chow@users.noreply.github.com>
2022-03-21 17:49:47 -0700

    added pet environment

--- src/main/ui/menus/ingame/GameMenu.java
+++ src/main/ui/menus/ingame/GameMenu.java
@@ -7,8 +7,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
+import static ui.configurables.InterfaceAesthetics.*;
+import static ui.menus.JComponentBuilder.*;
+
 // menu at the start of the game
 public class GameMenu extends Menu {
+    private JPanel topBarButtonContainer;
+    private JPanel bottomBarButtonContainer;
+
+    private PetEnvironment petEnvironment;
 
     // EFFECTS: constructs the main menu
     public GameMenu(GuiApp ui, JLayeredPane menu) throws IOException, FontFormatException {
@@ -16,16 +23,90 @@ public class GameMenu extends Menu {
         initMenu();
     }
 
-    // EFFECTS: initializes assets to the main menu
-    public void initMenu() throws IOException, FontFormatException {
-
+    // EFFECTS: initializes assets to the game menu
+    protected void initMenu() throws IOException, FontFormatException {
+        ui.changeBackground("PixelPetBackground2.png");
+        generateTopBar();
     }
 
+    // EFFECTS: creates a prompt asking for the player's name
+    private void generateTopBar() throws IOException, FontFormatException {
+        ui.clearMenu();
+        createTopBarItems();
+        createEnvironmentContainer();
+        createBottomBarItems();
+
+        menu.add(topBarButtonContainer);
+        menu.add(petEnvironment);
+        menu.add(bottomBarButtonContainer);
+        ui.pack();
+    }
+
+    // EFFECTS: creates the top bar with main menu and settings buttons
+    private void createTopBarItems() throws IOException, FontFormatException {
+        JButton mainMenuButton = createJButton("Main Menu", "mainMenuClicked", this,
+                24f, 180, 50);
+        mainMenuButton.setBackground(BUTTON_COLOR);
+
+        JButton settingsButton = createJButton("Settings", "settingsClicked", this,
+                24f, 160, 50);
+        settingsButton.setBackground(BUTTON_COLOR);
+
+        topBarButtonContainer = createJPanel(GAME_BAR_UI_COLOR, width, 50);
+        topBarButtonContainer.setLayout(new GridBagLayout());
+        topBarButtonContainer.add(mainMenuButton);
+        topBarButtonContainer.add(Box.createHorizontalStrut(5));
+        topBarButtonContainer.add(settingsButton);
+    }
+
+    // EFFECTS: creates the pixel pet environment
+    private void createEnvironmentContainer() throws IOException {
+        petEnvironment = new PetEnvironment(ui, width - 100, height - 150);
+        petEnvironment.repaint();
+    }
+
+    // EFFECTS: creates the bottom bar with shop, inventory, and pet stats buttons
+    private void createBottomBarItems() throws IOException, FontFormatException {
+        JButton shopButton = createJButton("Shop", "shopClicked", this,
+                24f, 100, 50);
+        shopButton.setBackground(BUTTON_COLOR_2);
+
+        JButton inventoryButton = createJButton("Inventory", "inventoryClicked", this,
+                24f, 180, 50);
+        inventoryButton.setBackground(BUTTON_COLOR_2);
+
+        JButton petStatsButton = createJButton("Pet Stats", "petStatsClicked", this,
+                24f, 180, 50);
+        petStatsButton.setBackground(BUTTON_COLOR_2);
+
+        bottomBarButtonContainer = createJPanel(GAME_BAR_UI_COLOR, width, 50);
+        bottomBarButtonContainer.setLayout(new GridBagLayout());
+        bottomBarButtonContainer.add(inventoryButton);
+        bottomBarButtonContainer.add(Box.createHorizontalStrut(5));
+        bottomBarButtonContainer.add(petStatsButton);
+        bottomBarButtonContainer.add(Box.createHorizontalStrut(5));
+        bottomBarButtonContainer.add(shopButton);
+    }
+
+    // TODO:
     // EFFECTS: helper for actionPerformed; performs the desired action
     protected void performAction(String command, JComponent source)
             throws IOException, FontFormatException {
-        if (command.equals("")) {
-        } else if (command.equals("")) {
+        if (command.equals("mainMenuClicked")) {
+            System.out.println("prompting main menu");
+        } else if (command.equals("settingsClicked")) {
+            System.out.println("opening settings");
+        } else if (command.equals("shopClicked")) {
+            System.out.println("opening shop");
+        } else if (command.equals("inventoryClicked")) {
+            System.out.println("showing inventory");
+        } else if (command.equals("petStatsClicked")) {
+            System.out.println("showing pet status");
         }
+    }
+
+    // GETTER:
+    public PetEnvironment getPetEnvironment() {
+        return petEnvironment;
     }
 }
---
+++ src/main/ui/menus/ingame/PetEnvironment.java
@@ -0,0 +1,129 @@
+package ui.menus.ingame;
+
+import ui.app.GuiApp;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static model.configurables.FileLocations.iconsDir;
+import static model.configurables.FileLocations.sceneryDir;
+import static ui.configurables.InterfaceAesthetics.*;
+import static ui.menus.JComponentBuilder.createJPanel;
+
+// represents the habitable environment, containing the scene and the pet
+public class PetEnvironment extends JPanel {
+    private GuiApp ui;
+    private int width;
+    private int height;
+
+    private JLabel scene;
+
+    private JPanel pet;
+    private JLabel petSprite;
+//    private Polygon boundary;
+
+    // EFFECTS: constructs the pet environment
+    public PetEnvironment(GuiApp ui, int width, int height) throws IOException {
+        this.ui = ui;
+        this.width = width;
+        this.height = height;
+//        makeBoundary();
+
+        super.setBackground(TRANSPARENT);
+        super.setPreferredSize(new Dimension(width, height));
+        super.setMaximumSize(new Dimension(width, height));
+        super.setLayout(new GridBagLayout());
+
+        scene = new JLabel();
+        scene.setMaximumSize(new Dimension(width, height));
+        scene.setLayout(null);
+        super.add(scene);
+
+        pet = createJPanel(TRANSPARENT, 100, 100);
+        pet.setPreferredSize(new Dimension(100, 100));
+        scene.add(pet);
+
+        translatePet(0,55);
+        changeScenery("PixelPetRoom.png");
+        drawPetSprite();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the scene image (the room)
+    public void changeScenery(String fileName) throws IOException {
+        BufferedImage bufferedImg = ImageIO.read(new File(sceneryDir + fileName));
+        Image img = new ImageIcon(bufferedImg).getImage()
+                .getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        scene.setIcon(new ImageIcon(img));
+    }
+
+    // EFFECTS: draws the pet sprite
+    private void drawPetSprite() throws IOException {
+        BufferedImage bufferedImage;
+        Image petImg;
+
+        bufferedImage = ImageIO.read(new File(iconsDir + "PixelPet.png"));
+        petImg = new ImageIcon(bufferedImage).getImage()
+                .getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+
+        try {
+            bufferedImage = ImageIO.read(new File(ui.getGame().getPet().getSpritesDir() + "Idle.png"));
+            petImg = new ImageIcon(bufferedImage).getImage()
+                    .getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            System.err.println("Unable to load default pet sprite");
+        }
+
+        petSprite = new JLabel(new ImageIcon(petImg));
+        pet.add(petSprite);
+    }
+
+    // EFFECTS: draws the pet sprite
+    private void updatePetSprite(String spriteFile) {
+        try {
+            BufferedImage bufferedImage =
+                    ImageIO.read(new File(ui.getGame().getPet().getSpritesDir() + spriteFile));
+            Image petImg = new ImageIcon(bufferedImage).getImage()
+                    .getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            petSprite = new JLabel(new ImageIcon(petImg));
+        } catch (IOException e) {
+            System.err.println("Unable to load pet sprite: " + spriteFile);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: translates the pet's location starting at the center of the room
+    private void translatePet(int x, int y) {
+        Insets insets = scene.getInsets();
+        pet.setBounds((width - pet.getPreferredSize().width) / 2 + insets.left + x,
+                (height - pet.getPreferredSize().height) / 2 + insets.top + y,
+                pet.getPreferredSize().width, pet.getPreferredSize().height);
+    }
+
+    // TODO:
+    // MODIFIES: this
+    // EFFECTS: updates the environment's graphics
+    public void renderGraphics() {
+
+    }
+
+//    // EFFECTS: creates the environment boundary
+//    public void makeBoundary() {
+//        boundary = new Polygon();
+//        boundary.addPoint(-100, -100);
+//        boundary.addPoint(-100, 100);
+//        boundary.addPoint(100, 100);
+//        boundary.addPoint(100, -100);
+//    }
+
+//    @Override
+//    public void paintComponent(Graphics graphics) {
+//        super.paintComponent(graphics);
+//        graphics.setColor(Color.WHITE);
+//        graphics.drawPolygon(boundary);
+//    }
+}
